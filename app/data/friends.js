a45efab1a662b2baa1ee2545e19192e9ae1ce476
blarsonmoore <blarsonmoore@gmail.com>
2018-07-21 22:39:04 -0600

    push

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friendsArray = [
+  {
+    name: "Gus",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["1", "5", "5", "1", "5"]
+  },
+  {
+    name: "Berry",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["2", "4", "1", "1", "1"]
+  },
+  {
+    name: "Sasha",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["4", "3", "5", "1", "1"]
+  },
+  {
+    name: "Britney",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["5", "1", "2", "1", "3"]
+  },
+  {
+    name: "Jessica",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["1", "1", "5", "5", "5"]
+  },
+  {
+    name: "Penny",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["1", "1", "5", "1", "3"]
+  },
+  {
+    name: "Jonny",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["5", "4", "4", "2", "4"]
+  },
+  {
+    name: "Abdual",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["5", "2", "3", "1", "5"]
+  },
+  {
+    name: "Nelson",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["5", "5", "5", "5", "5"]
+  },
+  {
+    name: "Betty",
+    photo: "http://via.placeholder.com/300x300",
+    scores: ["1", "1", "1", "1", "1"]
+  }
+];
+
+module.exports = friendsArray;
